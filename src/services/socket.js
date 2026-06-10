4a51249e05358569b88a1a3a1cb5dab700f20e72
pvivek8676 <pvivek8789@gmail.com>
2026-06-11 04:29:10 +0530

    update production API and socket url

--- src/services/socket.js
+++ src/services/socket.js
@@ -1,7 +1,7 @@
 import { io } from "socket.io-client";
 
 const SOCKET_URL =
-  "http://localhost:5000";
+  "https://real-time-chat-app-backend-x5x1.onrender.com";
 
 export const socket = io(
   SOCKET_URL,
